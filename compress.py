ce8f151ba35a0d1710cf7239913b2bb509a067e8
Josh Snyder <josh@code406.com>
2017-11-20 20:11:33 -0800

    GCS was fun, but let's stick with S3
    The GCS client library basically needs to either read the file into memory, or
    seek through it. That won't work here.

--- compress.py
+++ compress.py
@@ -1,23 +1,29 @@
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 from binascii import hexlify
-from google.cloud import storage
+import boto3
 from os import pipe
 import io
 import lz4.block
 import struct
 import sys
+import tempfile
 from time import time
 from multiprocessing import Process
 
-def encrypt(bufs):
-    key = ChaCha20Poly1305.generate_key()
-    yield key
-    chacha = ChaCha20Poly1305(key)
-    counter = 0
-    for buf in bufs:
-        nonce = struct.pack('<Qxxxx', counter)
-        counter += 1
-        yield chacha.encrypt(nonce, buf, None)
+class Encryptor(object):
+    def __init__(self, key):
+        self._aead = ChaCha20Poly1305(key)
+        self._counter = 0
+
+    def process(self, buf):
+        nonce = struct.pack('<Qxxxx', self._counter)
+        self._counter += 1
+        return self._aead.encrypt(nonce, buf, None)
+
+    @classmethod
+    def new(cls):
+        key = ChaCha20Poly1305.generate_key()
+        return key, cls(key)
 
 def compress(buf):
     return lz4.block.compress(buf, store_size=False)
@@ -25,7 +31,7 @@ def compress(buf):
 def decompress(buf):
     return lz4.block.decompress(buf, len(buf))
 
-def compress_loop(meta):
+def compress_loop():
     block_num = 0
     in_count = 0
     out_size = 0
@@ -35,23 +41,26 @@ def compress_loop(meta):
         if not in_data:
             break
         out_data = compress(in_data)
-        yield out_data
         out_size = len(out_data)
-        meta.append((block_num, in_count, out_size))
+        yield in_count, out_data
         in_count += len(in_data)
         block_num += 1
 
-def upload(label, reader, writer):
-    client = storage.Client(project='backups')
-    bucket = client.get_bucket('backups-echi7ma0au5kuca')
-    blob = bucket.blob(label)
 
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('backups-kehu504bfzn5ook')
+
+def upload(label, reader, writer):
     writer.close()
-    blob.upload_from_file(reader)
+    blob = bucket.Object(label)
+    blob.upload_fileobj(reader)
 
 meta = []
-encryptor = encrypt(compress_loop(meta))
-key = next(encryptor)
+key, e = Encryptor.new()
+meta_file = tempfile.TemporaryFile(dir='/tmp/')
+meta_file.write(hexlify(key))
+out_pos = 0
+
 (r, w) = pipe()
 reader = io.open(r, 'rb')
 writer = io.open(w, 'wb')
@@ -59,28 +68,23 @@ label = '{:016x}'.format(int(time() * 1e9))
 p = Process(target=upload, args=(label,reader,writer))
 del reader
 p.start()
-for o in encryptor:
-    writer.write(o)
+
+for block_num, d in enumerate(compress_loop()):
+    in_pos, buf = d
+    buf = e.process(buf)
+    writer.write(buf)
+    size = len(buf)
+    meta_file.write('{} {} {} {}\n'.format(block_num, in_pos, out_pos, size).encode('ascii'))
+    out_pos += size
+
 writer.close()
 p.join()
 sys.stderr.write('Finished uploading data\n')
-out = 0
-meta_file = io.BytesIO()
-meta_file.write(hexlify(key))
-meta_file.write(b'\n')
-for l in meta:
-    num, in_, size = l
-    # add authentication tag
-    size += 16
-    meta_file.write('{} {} {} {}\n'.format(num, in_, out, size).encode('ascii'))
-    out += size
+
 meta_file.seek(0)
-client = storage.Client(project='backups')
-bucket = client.get_bucket('backups-echi7ma0au5kuca')
-meta_blob = bucket.blob(label + '.meta')
-encryptor = encrypt([meta_file.getvalue()])
-key = next(encryptor)
-data = next(encryptor)
-meta_blob.upload_from_string(data)
+meta_blob = bucket.Object(label + '.meta')
+key, encryptor = Encryptor.new()
+data = encryptor.process(meta_file.read())
+meta_blob.put(Body=data)
 sys.stdout.buffer.write(hexlify(key))
-sys.stdout.write('\n')
+sys.stdout.write(' {}\n'.format(label))
